043288aed64caedad271a65e06c5b94067db7f24
Andrei Luca <andrei@andreiluca.com>
2015-08-11 02:15:27 +0300

    Clean things up

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 $blog_name = '<a href="'.get_bloginfo('url').'">'.get_bloginfo('name').'</a>';
 printf(__('Copyright %s %s %s &middot; Powered by %s <br/>','lightword'),'&copy;',date('Y'),$blog_name,'<a href="http://wordpress.org/" title="WordPress" target="_blank">WordPress</a>')
 ;?>
-<?php _e("<a href=\"http://www.lightworddesign.net/\" target=\"_blank\" title=\"LightWord Theme\">LightWord Theme</a> by Andrei Luca",'lightword');?>
+<?php _e("LightWord Theme by Andrei Luca",'lightword');?>
 </em>
 <?php global $lw_footer_content; echo $lw_footer_content; ?>
 <a title="<?php _e('Go to top','lightword'); ?>" class="top" href="#top"><?php _e('Go to top','lightword'); ?> &uarr;</a>
